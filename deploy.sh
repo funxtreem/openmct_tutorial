ece34b767a05fc489354fe4a746977142d4cf160
funxtreem <147969331+funxtreem@users.noreply.github.com>
2023-10-18 13:47:30 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash x
-hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
-docker run -d -p 80:8080 --name openmct-"$hostname" mvkaran/openmct:"$hostname"
+docker run -d -p 80:8080 --name openmct-"myhost" mvkaran/openmct:"myhost"
